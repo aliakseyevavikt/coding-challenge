67c91e53c216a5d1c4d366875cb00dd1d239fc2e
Viktoryia A <alekseevavikt@gmail.com>
2020-04-21 23:11:16 -0700

    isPrime

---
+++ JavaScript/isPrime.js
@@ -0,0 +1,12 @@
+// is this number a Prime? 1 - is not a Prime
+
+function isPrime(n){
+    if (n === 1) return false;
+    let count = 0;
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {count++;}
+    }
+    return count === 0;
+}
+console.log(isPrime(80));
+console.log(isPrime(13));
