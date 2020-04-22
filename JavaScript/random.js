db678740989337f0d0a250a8fb5d6040d1a397ef
Viktoryia A <alekseevavikt@gmail.com>
2020-04-21 23:34:33 -0700

    new challenges for the future

---
+++ JavaScript/random.js
@@ -0,0 +1,16 @@
+// 1.
+// Resolve: 7321 = 7+3+2+1 = 13 = 1+3 = 4 (the answer should be onr digit number)
+
+
+
+
+// 2. “word is word” - how much times do you have every word?
+
+
+
+//3. Раскрыть многоуровневый array
+
+
+
+//4. Your have num "x". Return array of numbers from 0 to x,
+// that could be divided on 3 and 7 at the same time
