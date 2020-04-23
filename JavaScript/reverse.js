f9ed8a43633b5c7c8d225d2a47d7adf6590f0e96
Viktoryia A <alekseevavikt@gmail.com>
2020-04-22 23:40:56 -0700

    reverse

---
+++ JavaScript/reverse.js
@@ -0,0 +1,10 @@
+// reverse a string (letters)
+// version 1.0
+
+function reverse (str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverse("Hello World"));
+
+
+//reverse words in the string
