a3d1e3e1391fa5e61472d990909f271e68f26823
Viktoryia A <alekseevavikt@gmail.com>
2020-04-22 23:31:25 -0700

    factorial task version 2.0

--- JavaScript/factorial.js
+++ JavaScript/factorial.js
@@ -1,5 +1,9 @@
-//factorial of n (n!) = 1 * 2 * 3 * 4 * ... * n
+//factorial of n (n!) = n * ... * 4 * 3 * 2 * 1;
+// factorial n! = n * (n -1 ) * (n -2) * ... * 2 * 1;
+// n! = n * (n - 1)!
 
+
+//version 1.0
 function factorial(num) {
     let fact = 1;
     for (let i = 1; i <= num; i++) {
@@ -8,3 +12,14 @@ function factorial(num) {
       return fact;
 }
 console.log(factorial(4))
+
+//version 2.0
+
+function factorialRecursion (n) {
+   if (n === 1) {
+     return 1
+   }
+     return   n * factorialRecursion(n - 1);
+ }
+
+console.log(factorialRecursion(4))
