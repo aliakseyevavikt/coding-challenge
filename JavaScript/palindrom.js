2215eb3bde7fcf05fd6ce752b605688b235b95fe
Viktoryia A <alekseevavikt@gmail.com>
2020-04-28 21:36:27 -0700

    palindrom.js

--- JavaScript/palindrom.js
+++ JavaScript/palindrom.js
@@ -1,18 +1,52 @@
-// Is this word a palindrom?
-// version 1.0: same case, without spaces
-// Example: madam
+// Is this a palindrom?
+// task 1.  same case, without spaces (one word)
+// version 1.0. with methods
 
+console.log("task 1 v. 1.0. Result: true, false ");
 function palindrom (str) {
     let strPalindrom = str.split('').reverse().join('');
     return str === strPalindrom;
 }
 
-console.log(palindrom("madam im adam"));
+console.log(palindrom("madam"));
 console.log(palindrom("madama"));
 
 //version 2.0: without methods
+console.log();
+console.log("task 1 v. 2.0. Result: true, false ");
 
-//version 3.0: different cases
+function palindrom1(str1){
+    let strNew = '';
+    for (let i = 0; i < str1.length; i++) {
+        strNew = str1[i] + strNew;
+    }
+    return strNew === str1;
+}
+console.log(palindrom1('madam'));
+console.log(palindrom1('madan'));
+
+//task 2: different cases
 
 
-// version 4.0 different cases, spaces
+// task 3: without space
+// version 1.0 without methods
+console.log();
+console.log("task 3 v. 1.0. Result: true, true, false");
+
+function isPalindrome(str3){
+    let strNoSpace = '';
+    let strReverse = '';
+    for (let i = 0; i < str3.length; i++) {
+        if (str3[i] !== ' ') {
+            strNoSpace += str3[i];
+            strReverse = str3[i] + strReverse;
+        }
+    }
+    return strNoSpace === strReverse;
+}
+console.log(isPalindrome("was it a car or a cat i saw"));
+console.log(isPalindrome("pull up if i pull up"));
+console.log(isPalindrome("pulll up if i pull up"));
+
+
+// version 5.0 different cases, spaces
