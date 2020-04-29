9f386ec0a7b35f62f49232d534e8fd09c7575b87
Viktoryia A <alekseevavikt@gmail.com>
2020-04-28 21:18:16 -0700

    reverse.js

--- JavaScript/fibonacci.js
+++ JavaScript/fibonacci.js
@@ -1,4 +1,47 @@
-//Fibonacci
+//Fibonacci 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...
+//task 1. return N elements Fibonacci as array
 
-//return N element Fibonacci
+console.log("task 1");
 
+function fibonacci(n){
+    if (n === 0) return [];
+    if (n === 1) return [0];
+    let res = [0, 1];
+    for (let i = 2; i < n; i++) {
+        res.push(res[res.length-1] + res[res.length -2]);
+    }
+    return res;
+}
+console.log(fibonacci(10)); //[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+//task 2. Return Nth element Fibanacci;
+// version 1.0
+
+console.log("task 2. V. 1.0");
+
+function nthFibonacci(n){
+    if (n === 1) return 0;
+    if (n === 2) return 1;
+    let elem1 = 0, elem2 = 1;
+    for (let i = 3; i <= n; i++) {
+        let temp = elem1;
+        elem1 = elem2;
+        elem2 = elem1 + temp;
+    }
+    return elem2;
+}
+console.log(nthFibonacci(10)); // 34
+
+// version 2.0
+
+console.log("task 2. V. 2.0");
+
+function nthFibonacci1(n1){
+    let arr = [0, 1];
+    for (let i = 2; i < n1; i++) {
+        arr.push(arr[arr.length-1] + arr[arr.length -2]);
+    }
+    return arr[n1 -1];
+}
+
+console.log(nthFibonacci1(10)); //34
--- JavaScript/reverse.js
+++ JavaScript/reverse.js
@@ -2,15 +2,18 @@
 // "Hello world" => "dlrow olleH"
 
 // version 1.0 with methods
-console.log("task 1 v. 1.0. *********************");
+
+console.log("task 1 v. 1.0. Result: !dlroW olleH");
 
 function reverse (str) {
     return str.split('').reverse().join('');
 }
-console.log(reverse("Hello World"));
+console.log(reverse("Hello World!"));
 
 //version 2.0 without methods
-console.log("task 1 v. 2.0 *********************");
+
+console.log();
+console.log("task 1 v. 2.0. Result: !dlroW iH");
 
 function reverse1 (str1) {
     let newStr = '';
@@ -19,12 +22,26 @@ function reverse1 (str1) {
     }
     return newStr;
 }
+console.log(reverse1('Hi World!'))
 
-console.log(reverse1('abcdefg'))
+//version 3.0 without methods
 
+console.log();
+console.log("task 1 v. 3.0. Result: !enoyreve olleH");
+
+function reverse11 (str2) {
+    let newStr2 = '';
+    for (let i = 0; i < str2.length; i++) {
+        newStr2 = str2[i] + newStr2;
+    }
+    return newStr2;
+}
+console.log(reverse11('Hello everyone!'))
 
 // task 2: reverse words in the string
-console.log("task 2 *********************");
+
+console.log();
+console.log("task 2 *");
 
 function reverse2 (str3) {
     return str3.split(' ').reverse().join(' ');
@@ -32,12 +49,18 @@ function reverse2 (str3) {
 console.log(reverse2("Hello my dear World"));
 
 // task 3: reverse only vowels in the string
-console.log("task 3 *********************");
+
+console.log();
+console.log("task 3");
 
 // task 4: reverse array without method reverse
 
 // version 1.0 (push)
-console.log("task 4 v. 1.0 *********************");
+
+console.log();
+console.log("task 4 v. 1.0");
 
 // version 2.0 (without any methods)
-console.log("task 4 v. 2.0 *********************");
+
+console.log();
+console.log("task 4 v. 2.0");
