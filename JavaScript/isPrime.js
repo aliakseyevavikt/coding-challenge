580ba550c8687f3f6c96cff8efb011ab32c66507
Viktoryia A <alekseevavikt@gmail.com>
2020-05-11 11:36:59 -0700

    isPrime

--- JavaScript/isPrime.js
+++ JavaScript/isPrime.js
@@ -1,17 +1,53 @@
-// is this number a Prime? 1 - is not a Prime
-// no one integer cannot be divided on integer that more than it's half
-// so we should check integers from 2 to n/2
+// isPrime - > Prime number can be divided only on 1 and itself.
+// But 1 - is not a Prime
+// ! no one integer cannot be divided on integer that more than it's half
+// => so we should check integers from 2 to n/2
 
-function isPrime(n){
-    if (n === 1) return false;
+//TAsk 1: is "num" Prime?
+
+function isPrime(num) {
+    if (num === 1) return false;
     let count = 0;
-    for (let i = 2; i <= n/2; i++) {
-        if (n % i === 0) {count++;}
+    for (let i = 2; i <= num / 2; i++) {
+        if (num % i === 0) count++;
     }
     return count === 0;
 }
-console.log(isPrime(80));
-console.log(isPrime(89));
+    //another version of isPrime function:
+    //         function isPrime(num){
+    //             let count = 0;
+    //             for (let i = 2; i <= num/2; i++) {
+    //                 if (num % i === 0) count++
+    //             }
+    //             return count === 0 && num !== 1;
+    //         }
 
-//Return all primes from 1 till 100 as array
+console.log(isPrime(8));
+console.log(isPrime(11));
+
+//TASK 2: Return all primes from n till m as array
+
+//version 1. We will use 2 function: isPrime and second one that creates array
+
+function primeNumbers(n,m){
+    let arr = [];
+    for (let i = n; i <= m; i++) {
+        if (isPrime(i)) arr.push(i);
+    }
+    return arr;
+}
+
+console.log(primeNumbers(2, 50));
+
+//TASK 3. How much primes from n1 till m1?
+
+function countOfPrimeNumbers(n, m){
+    let count = 0;
+    for (let i = n; i <= m; i++) {
+        if (isPrime(i)) count++;
+    }
+    return count;
+}
+
+console.log(countOfPrimeNumbers(1, 90));
 
