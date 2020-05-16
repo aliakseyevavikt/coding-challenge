119e64d086606d9e784a994599d0aeec275301f7
Viktoryia A <alekseevavikt@gmail.com>
2020-05-16 15:36:28 -0700

    add stairs

--- JavaScript/fibonacci.js
+++ JavaScript/fibonacci.js
@@ -19,16 +19,16 @@ console.log(fibonacci(10)); //[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
 console.log("task 2. V. 1.0");
 
-function nthFibonacci(n){
-    if (n === 1) return 0;
-    if (n === 2) return 1;
-    let elem1 = 0, elem2 = 1;
+function nthFibonacci(n) {
+    let f1 = 0;
+    let f2 = 1;
+    let f3;
     for (let i = 3; i <= n; i++) {
-        let temp = elem1;
-        elem1 = elem2;
-        elem2 = elem1 + temp;
+        f3 = f2 + f1;
+        f1 = f2;
+        f2 = f3;
     }
-    return elem2;
+    return n === 1? f1: n === 2? f2: f3;
 }
 console.log(nthFibonacci(10)); // 34
 
@@ -46,6 +46,14 @@ function nthFibonacci1(n1){
 
 console.log(nthFibonacci1(10)); //34
 
+//version 3
+
+function fib(n) {
+    let [x, y] = [0, 1];
+    for (let i = 1; i < n; i++) [x, y] = [y, x + y];
+    return x;
+}
+
 // task 3.
 
 // Given that
@@ -53,10 +61,8 @@ console.log(nthFibonacci1(10)); //34
 // f0 = '0'
 // f1 = '01'
 // f2 = '010' = f1 + f0
-// f3 = '01001' = f2 + f1
 // You will be given a number and your task is to return the nth fibonacci string. For example:
 //
-//     solve(2) = '010'
 // solve(3) = '01001'
 
 function solve(n){
@@ -73,5 +79,11 @@ function solve(n){
     return n === 0? f0: n=== 1? f1: f2;
 }
 
+//version2
+function solve1(n){
+    if(n==0) return '0';
+    else if(n==1) return '01';
+    return solve1(n-1)+solve1(n-2);
+}
 
 // task 4.
--- JavaScript/stars.js
+++ JavaScript/stars.js
@@ -111,3 +111,140 @@ console.log(drawTriangle(4));
 // function starsReverse(n) {
 //
 // }
+
+function drawTriangle(n) {
+//   let str = '';
+//   for (let i = n; i >= 1; i--) {
+//     str += '*'.repeat(i);
+//     if (i !== 1) str += '\n';
+//   }
+//   return str;
+// }
+
+    function drawTriangle(n) {
+        let str = '';
+        for (let i = n; i >= 1; i--) {
+            for (let j = 1; j <= i; j++) {
+                str += '*';
+            }
+            if (i !== 1) str += '\n';
+        }
+        return str;
+    }
+
+    console.log(drawTriangle(4));
+
+ //   Напишите функцию drawTriangle(n) которая
+// принимает ширину треугольника (здесь 7) n и возвращает строку,
+// содержащую рисунок:
+//     *
+//    ***
+//   *****
+//  *******
+
+// function pattern(n){
+
+
+
+// }
+// console.log(pattern(7));
+
+
+//5 Напишите функцию, которая принимает количество ступенек n (n = 5)
+// и рисует лесенку:
+// #
+// _#
+// __#
+// ___#
+// ____#
+
+// function pattern(n){
+
+
+
+// }
+// console.log(pattern(7));
+
+//6 Узор из чисел: (n  чисел в строке, m строк)
+// 11111
+// 22222
+// 33333
+// 44444
+
+// function pattern(n, m){
+
+
+
+// }
+// console.log(pattern(5, 4));
+
+
+//7 Треугольник из чисел (n - количество строк)
+
+// "1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15"
+
+
+// function digitTriangle(n){
+
+// }
+
+// console.log(digitTriangle(10));
+
+
+//8 Walk-down Stairs
+// "1 1
+// 1 2 2 1
+// 1 2 3 3 2 1
+// 1 2 3 4 4 3 2 1
+// 1 2 3 4 5 5 4 3 2 1
+// 1 2 3 4 5 6 6 5 4 3 2 1 "
+
+
+// function stairs(n){
+
+// }
+
+// console.log(stairs(6));
+
+
+// TASK 9 Walk-up Stairs
+// https://www.codewars.com/kata/566c3f5b9de85fdd0e000026/train/javascript
+
+// "
+//                                     1 1
+//                                 1 2 2 1
+//                             1 2 3 3 2 1
+//                         1 2 3 4 4 3 2 1
+//                     1 2 3 4 5 5 4 3 2 1
+//                 1 2 3 4 5 6 6 5 4 3 2 1
+//             1 2 3 4 5 6 7 7 6 5 4 3 2 1
+//         1 2 3 4 5 6 7 8 8 7 6 5 4 3 2 1
+//     1 2 3 4 5 6 7 8 9 9 8 7 6 5 4 3 2 1
+// 1 2 3 4 5 6 7 8 9 0 0 9 8 7 6 5 4 3 2 1"
+
+
+    function stairs(n) {
+        let str = '';
+        for (let i = 1; i <= n; i++) {
+            str += ' '.repeat(n * 4 - 2 - (i * 4 - 2));
+            for (let j = 1; j <= i; j++) {
+                str = (j < 10)?  str + j + ' ': str + j % 10 + ' ';
+            }
+            for (let j = i; j >= 1; j--) {
+                if (j < 10) str = (j !==1)? str + j + ' ': str + j;
+                else {
+                    let temp = j % 10;
+                    str += temp + ' ';
+                }
+            }
+            if (i != n) str += '\n';
+        }
+        return str;
+    }
+
+
+ console.log(stairs(10));
