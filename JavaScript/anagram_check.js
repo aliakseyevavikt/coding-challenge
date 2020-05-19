fa304b1f4013b50b80d973716fc70c3deaaba298
Viktoryia A <alekseevavikt@gmail.com>
2020-05-18 21:55:05 -0700

    patches

---
+++ JavaScript/anagram_check.js
@@ -0,0 +1,12 @@
+// Anagrams are words or phrases you spell by rearranging the letters of another word or phrase.
+// Task 1. Is this word anagram? night = thing
+
+//version 1 with methods
+
+
+//version 2 without methods
+
+
+//Task2
+//  'eleven plus two = twelve plus one'
+
