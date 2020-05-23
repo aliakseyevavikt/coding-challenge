f7a3ef6340f9a06ac642ceedab6c7bdd9e9c7811
Viktoryia A <alekseevavikt@gmail.com>
2020-05-22 22:35:26 -0700

    multiple

--- JavaScript/multiple.js
+++ JavaScript/multiple.js
@@ -1,4 +1,15 @@
-/*Task:
-Return array of numbers from start to x, that could be divided on 3 and 7 at the same time
+/*TASK:
+Return array of numbers from start to end, that could be divided on 3 and 7 at the same time
 */
 
+function multiple (start, end) {
+    let arr = [];
+    for (let i = start; i <= end; i++) {
+        if(i % 3 === 0 && i % 7 === 0) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+
+console.log(multiple(3, 100));
--- test/index.js
+++ test/index.js
@@ -1,8 +1,18 @@
-const {reverse, reverse1, reverse11} = require("../JavaScript/reverse_string")
+const {changeValue} = require("../JavaScript/change_variables_value");
+const {reverse, reverse1, reverse11} = require("../JavaScript/reverse_string");
+const {reverseWords} = require("../JavaScript/reverse_words_in_string");
+
 const expect = require('chai').expect;
 
+//change variable value
+it('change variable value 64 => 37', () => {
+    expect(changeValue(37, 64), 64, 38);
+})
+
+//reverse a string (all letters)
+
 it('reverse a string version 1.0', () => {
-    expect(reverse("Hello"), "olleH");
+    expect(reverse("Hello"), "ollлллллeH");
 })
 
 it('reverse a string version 2.0', () => {
@@ -10,6 +20,11 @@ it('reverse a string version 2.0', () => {
 })
 
 it('reverse a string version 3.0', () => {
-    expect(reverse1("Hello everyone!"), "!enoyreve olleH");
+    expect(reverse11("Hello everyone!"), "!enoyreve olleH");
 })
 
+//reverse words in the string
+
+it('reverse words in the string', () => {
+    expect(reverseWords("Hello my dear World"), "World dear my Hello");
+})
