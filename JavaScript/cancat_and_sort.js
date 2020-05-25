a7ae05427aa79116a6445b26b955911743b15c35
Viktoryia A <alekseevavikt@gmail.com>
2020-05-24 23:27:15 -0700

    cancat_and_sort added solution

--- JavaScript/cancat_and_sort.js
+++ JavaScript/cancat_and_sort.js
@@ -16,15 +16,39 @@ function contatAndSort1(arr1, arr2){
         arr1.push(arr2[i]);
     }
 
-
-
+for (let i = 0; i < arr1.length; i++){
+    for (let j = i + 1; j < arr1.length; j++) {
+    if (arr1[i] > arr1[j]) {
+        let temp = arr1[i];
+        arr1[i] = arr1[j];
+        arr1[j] = temp;
+    }
+    }
+}
     return arr1;
 }
 console.log(contatAndSort1([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]));
 
+//TASK3: Arrays could be different lengths + we cannot use any methods push;
 
+function contatAndSort2(arr1, arr2){
+    let start = arr1.length;
+    for (let i = start, j = 0; j < arr2.length; i++, j++){
+        arr1[i] = arr2[j];
+    }
 
-//TASK3: Arrays could be different lengths;
+    for (let i = 0; i < arr1.length; i++){
+        for (let j = i + 1; j < arr1.length; j++) {
+            if (arr1[i] > arr1[j]) {
+                let temp = arr1[i];
+                arr1[i] = arr1[j];
+                arr1[j] = temp;
+            }
+        }
+    }
+    return arr1;
+}
+console.log(contatAndSort2([1, 3, 5, 7, 9, 11, 13, 15], [2, 4, 6, 8, 10, 12]));
 
 
 
--- JavaScript/sort.js
+++ JavaScript/sort.js
@@ -7,8 +7,10 @@ function sorting(arr){
 console.log(sorting([2, 5, 7, 1, 9, -7, -99, 56]));
 
 //version without methods
+//idea - we take element and check if it's minimum. if arr[i] bigger, we will reassign it
+// so each time we try to find minimum
 function sorting2(arr){
-    for (let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length - 1; i++){
         for (let j = i+1; j < arr.length; j++) {
             if (arr[j] < arr[i]) {
                 let temp = arr[i];
