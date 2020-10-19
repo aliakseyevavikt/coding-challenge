16e1d755c089ff41dfd0ffd358942e79ed7164d6
Viktoryia A <alekseevavikt@gmail.com>
2020-10-18 19:57:18 -0700

    add version to sorting

--- JavaScript/sort.js
+++ JavaScript/sort.js
@@ -23,3 +23,26 @@ function sorting2(arr){
 }
 
 console.log(sorting2([2, 5, 7, 1, 9, -7, -99, 56]));
+
+//version 2 (we do not change elements each time)
+function sorting3(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = arr[i];
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (min > arr[j]) {
+                min = arr[j];
+                minIndex = j
+            }
+            if (arr[i] !== min) {
+                let temp = min;
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(sorting3([30, 50, 20, 10, 40]));
+
