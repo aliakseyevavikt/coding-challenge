e9013d6aa1b73884d664c427c452d6b443c2be9c
Viktoryia A <alekseevavikt@gmail.com>
2020-10-29 12:07:54 -0700

    added new solutions

--- JavaScript/reverse_string.js
+++ JavaScript/reverse_string.js
@@ -1,16 +1,24 @@
-// TASK: reverse a string (all letters) Example: "Hello world" => "dlrow olleH"
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//
 
 // version 1.0 with methods
 
-function reverse (str) {
+function reverse(str) {
     return str.split('').reverse().join('');
 }
 
 //version 2.0 without methods
 
-function reverse1 (str) {
+function reverse2(str) {
     let newStr = '';
-    for (let i = str.length - 1; i >= 0; i-- ) {
+    for (let i = str.length - 1; i >= 0; i--) {
         newStr += str[i];
     }
     return newStr;
@@ -18,10 +26,16 @@ function reverse1 (str) {
 
 //version 3.0 without methods
 
-function reverse11 (str) {
+function reverse3(str) {
     let newStr = '';
-    for (let i = 0; i < str.length; i++) {
-        newStr = str[i] + newStr;
+    for (let char of str) {
+        newStr = char + newStr;
     }
     return newStr;
+}
+
+//version 4.0. with reduce
+
+function reverse3(str) {
+    return str.split('').reduce((res, char) => char + res, '');
 }
