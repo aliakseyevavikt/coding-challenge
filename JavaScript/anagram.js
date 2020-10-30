c4be7c797f1aadb2d6b60d1b872f4ce394311a65
Viktoryia A <alekseevavikt@gmail.com>
2020-10-29 21:11:53 -0700

    anagram.js

--- JavaScript/anagram.js
+++ JavaScript/anagram.js
@@ -1,9 +1,18 @@
-// Anagrams are words or phrases you spell by rearranging the letters of another word or phrase.
-// Task 1. Is this word/phrases anagram? night = thing
+/* Directions
+Check to see if two provided strings are anagrams of eachother.
+One string is an anagram of another if it uses the same characters
+in the same quantity. Only consider characters, not spaces
+or punctuation.  Consider capital letters to be the same as lower case
 
-//version 1 "is the word anagram?"
+  anagrams('rail safety', 'fairy tales') --> True
+  anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+  anagrams('Hi there', 'Bye there') --> False
 
-function anagram (str1, str2) {
+ */
+
+// version 1 (words without spaces)
+
+function anagram(str1, str2) {
     return str1.split('').sort().join('') === str2.split('').sort().join('');
 }
 
@@ -12,23 +21,6 @@ console.log(anagram('neght', 'thing'));
 
 //  version 2 "is the phrase anagram?"
 
-function anagram1 (str1, str2) {
-    str1 = str1.replace(/' '/g, '')
-        .split('')
-        .sort()
-        .join('');
-    str2 = str2.replace(/' '/g, '')
-        .split('')
-        .sort()
-        .join('');
-    return  str1 === str2;
-}
-
-console.log(anagram1('eleven plus two', 'twelve plus one'));
-console.log(anagram1('eleven plas two', 'twelve plus one'));
-
-// version 3 "is the phrase with different case anagram?"
-
 function anagram2 (str1, str2) {
     str1 = str1.toLowerCase()
             .replace(/' '/g, '')
@@ -36,14 +28,62 @@ function anagram2 (str1, str2) {
             .sort()
             .join('');
     str2 = str2.toLowerCase()
-            .replace(/' '/g, '')
-            .split('')
-            .sort()
-            .join('');
-    return  str1 === str2;
+        .replace(/' '/g, '')
+        .split('')
+        .sort()
+        .join('');
+    return str1 === str2;
 }
 
 console.log(anagram2('elEven Plus Two', 'twelve pLus one'));
 console.log(anagram2('elevEn plas Two', 'twelve pLus one'));
 
+// version 3
+function anagram3(str1, str2) {
+    function reformString(str) {
+        return str.toLowerCase()
+            .replace(/' '/g, '')
+            .split('')
+            .sort()
+            .join('');
+    }
+
+    return reformString(str1) === reformString(str2);
+}
+
+console.log(anagram3('elEven Plus Two', 'twelve pLus one'));
+console.log(anagram3('elevEn plas Two', 'twelve pLus one'));
+
+// version 3 with objects
+
+function anagrams4(stringA, stringB) {
+
+    function createObj(str) {
+        str = str.replace(/[^a-z]/gi, '').toLowerCase();
+        let obj = {};
+        for (let i = 0; i < str.length; i++) {
+            if (obj.hasOwnProperty(str[i])) {
+                obj[str[i]]++;
+            } else {
+                obj[str[i]] = 1;
+            }
+        }
+        return obj;
+    }
+
+    let objA = createObj(stringA);
+    let objB = createObj(stringB);
+
+    if (Object.keys(objA).length !== Object.keys(objB).length) {
+        return false;
+    }
+
+    for (let key in objA) {
+        if (objB[key] !== objA[key]) return false;
+    }
+    return true;
+}
+
+console.log(anagrams4('he    llo', 'oLelh'));
+console.log(anagrams4('he    llo', 'oLelhs'));
 
