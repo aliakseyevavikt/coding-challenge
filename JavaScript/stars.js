80e6a51041f3768b1ef850cdee308bb911e5f11e
Viktoryia A <alekseevavikt@gmail.com>
2020-11-01 21:54:17 -0800

    stars.js => updated

--- JavaScript/stars.js
+++ JavaScript/stars.js
@@ -1,103 +1,124 @@
-//1 Напишите функцию pattern(n, m) которая
-// принимает размеры n, m и возвращает строку,
-// содержащую узор: (n звездочек в одном ряду, m рядов)
+// Directions:
+
+// TASK 1. Print picture:
 // **********
 // **********
 // **********
 // **********
 
-// function pattern(n, m) {
-//   let str = '';
-//   for (let i = 1; i <= m; i++) {
-//     for (let j = 1; j <= n; j++) {
-//       str += '*';
-//     }
-//     if (i !== m) str += '\n';
-//   }
-//   return str;
-// }
+function pattern(n, m) {
+    let str = '';
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            str += '*';
+        }
+        if (i !== m) str += '\n';
+    }
+    return str;
+}
 
-// function pattern(n, m) {
-//   let str = '';
-//   for (let i = 1; i <= m; i++) {
-//     str += '*'.repeat(n);
-//     if (i !== m) str += '\n';
-//   }
-//    return str;
-// }
+console.log(pattern(8, 5));
 
-// function pattern(n, m) {
-//     let str = (('*'.repeat(n)) + '\n').repeat(m-1) +
-//         '*'.repeat(n);
-//     return str;
-// }
-//
-// console.log(pattern(10, 4));
+function pattern1(n, m) {
+    let str = '';
+    for (let i = 1; i <= m; i++) {
+        str += '*'.repeat(n);
+        if (i !== m) str += '\n';
+    }
+    return str;
+}
 
-// 2Напишите функцию drawTriangle(n) которая
-// принимает количество строк n и возвращает строку,
-// содержащую рисунок:
+console.log("###########");
+console.log(pattern1(8, 5));
+
+function pattern2(n, m) {
+    let str = (('*'.repeat(n)) + '\n').repeat(m - 1) +
+        '*'.repeat(n);
+    return str;
+}
+
+console.log("###########");
+console.log(pattern2(8, 5));
+
+// TASK 2. Print picture:
 // *
 // **
 // ***
 // ****
 
+// version 1
+
 function drawTriangle(n) {
- let str = '*';
-  for (let i = 2; i <= n; i++) {
-    str += '\n' + '*'.repeat(i);
-  }
-  return str;
+    let res = '';
+    for (let i = 1; i <= n; i++) {
+        if (i !== n) {
+            res += '*'.repeat(i) + ' '.repeat(n - i) + '\n';
+        } else {
+            res += '*'.repeat(n);
+        }
+    }
+    return res;
 }
 
-console.log(drawTriangle(4));
+console.log("###########");
+console.log(drawTriangle(1));
+console.log("###########");
+console.log(drawTriangle(5));
 
-/* task2
-*
-**
-***
-****
-*****
-......
- */
+// version 2
 
-// // version 1.0 (without method repeat)
-// console.log("task 2 v. 1.0 *********************");
-//
-// function stars (num) {
-//     let str = '';
-//     for (let i = 1; i <= num; i++) {
-//         for (let j = 1; j <= i; j++) {
-//             str += '*';
-//         }
-//         if (i !== num) {
-//             str += '\n';
-//         }
-//     }
-//     return str;
-// }
-//
-// console.log(stars(7))
-//
-//
-// // version 2 (with method repeat)
-// console.log("task 2 v. 2.0 *********************");
-//
-// function stars2 (num2) {
-//     let str2 = '';
-//     for (let i = 1; i <= num2; i++) {
-//         str2 += '*'.repeat(i);
-//         if (i !== num2) {
-//             str2 += '\n';
-//         }
-//     }
-//     return str2;
-// }
-//
-// console.log(stars2(5))
-//
-//
-//
+// version 2 (with method repeat)
+
+function drawTriangle1(num) {
+    let res = '';
+    for (let i = 1; i <= num; i++) {
+        res += '*'.repeat(i);
+        if (i !== num) {
+            res += '\n';
+        }
+    }
+    return res;
+}
+
+console.log("###########");
+console.log(drawTriangle1(5))
+
+//  version 3 (without method repeat)
+
+function drawTriangle2(num) {
+    let str = '';
+    for (let i = 1; i <= num; i++) {
+        for (let j = 1; j <= i; j++) {
+            str += '*';
+        }
+        if (i !== num) {
+            str += '\n';
+        }
+    }
+    return str;
+}
+
+console.log("###########");
+console.log(drawTriangle2(7))
+
+// TASK 3
+//  steps(4) => console.log each string
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n) {
+    for (let i = 1; i <= n; i++) {
+        let res = '#'.repeat(i) + ' '.repeat(n - i);
+        console.log(res);
+    }
+}
+
+console.log("TASk 3 ###########");
+console.log(steps(8))
+
+
 // /* task3
 // ......
 // *****
@@ -112,29 +133,29 @@ console.log(drawTriangle(4));
 //
 // }
 
-function drawTriangle(n) {
-//   let str = '';
-//   for (let i = n; i >= 1; i--) {
-//     str += '*'.repeat(i);
-//     if (i !== 1) str += '\n';
-//   }
-//   return str;
-// }
+// function drawTriangle(n) {
+// //   let str = '';
+// //   for (let i = n; i >= 1; i--) {
+// //     str += '*'.repeat(i);
+// //     if (i !== 1) str += '\n';
+// //   }
+// //   return str;
+// // }
+//
+//     function drawTriangle(n) {
+//         let str = '';
+//         for (let i = n; i >= 1; i--) {
+//             for (let j = 1; j <= i; j++) {
+//                 str += '*';
+//             }
+//             if (i !== 1) str += '\n';
+//         }
+//         return str;
+//     }
+//
+//     console.log(drawTriangle(4));
 
-    function drawTriangle(n) {
-        let str = '';
-        for (let i = n; i >= 1; i--) {
-            for (let j = 1; j <= i; j++) {
-                str += '*';
-            }
-            if (i !== 1) str += '\n';
-        }
-        return str;
-    }
-
-    console.log(drawTriangle(4));
-
- //   Напишите функцию drawTriangle(n) которая
+//   Напишите функцию drawTriangle(n) которая
 // принимает ширину треугольника (здесь 7) n и возвращает строку,
 // содержащую рисунок:
 //     *
